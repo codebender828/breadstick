7612bb34408e438dbc04dfe404ff8b55a6fee7ed
codebender828 <jbakebwa@163.com>
2020-05-20 20:34:18 +0800

    refactor: export class instance without invocation

--- src/plugin/index.js
+++ src/plugin/index.js
@@ -1,11 +1,10 @@
 import Breadstick from '../components/Breadstick'
-const breadstick = new Breadstick()
 
 /**
  * Breadstick plugin
  */
 export default {
   install (Vue, options = {}) {
-    Vue.prototype.$breadstick = breadstick
+    Vue.prototype.$breadstick = new Breadstick()
   }
 }
